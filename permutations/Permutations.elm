6e1a548bcb8c1e1d5a9d5c480d959e3a18a82dd0
Kenneth Bogner <bognerkenny@gmail.com>
2017-04-20 00:01:31 -0400

    Made some minor adjustments to Permutations.elm

--- permutations/Permutations.elm
+++ permutations/Permutations.elm
@@ -1,13 +1,31 @@
-module Main exposing (..)
+module Permutations exposing (..)
 
-import Html exposing (..)
-import String
+import Html exposing (Html, text)
 
 
+main : Html a
 main =
-    permute [ 1, 2, 3 ]
+    permute [ 1, 2, 3, 4, 5 ]
         |> toString
         |> text
+
+
+
+{- }
+
+   The permutations are built up by taking each item in the list and calling `permute`
+   on the remaining list. So
+
+       [1,2,3] =
+           1 :: permute [2,3] =
+               2 :: permute [3] =
+                   3 :: []
+               3 :: permute [2] =
+                   2 :: []
+           2 :: permute [1,3]
+           3 :: permute [1,2]
+
+-}
 
 
 permute : List a -> List (List a)
